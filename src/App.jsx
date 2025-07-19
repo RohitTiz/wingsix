2303bfab24ae6e8a604faa87e4795f327dc64861
RohitTiz <rohittiwari06052004@gmail.com>
2025-07-19 11:19:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <p className='text-3xl text-amber-700'>hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
